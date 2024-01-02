b2f60e3a90daf3b27d47b24c6e9bc1fbc1073627
Partho Bhowmick <p.s.bhowmick@gmail.com>
2024-01-01 23:14:05 -0600

    actual-CRD
    updated CRD

--- api/v1alpha1/task_types.go
+++ api/v1alpha1/task_types.go
@@ -14,22 +14,33 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+// +kubebuilder:object:generate=true
 package v1alpha1
 
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	corev1 "k8s.io/client-go/applyconfigurations/core/v1"
+	v1 "k8s.io/client-go/applyconfigurations/meta/v1"
 )
+
+// +kubebuilder:object:root=true
+type TaskLabelSelectorApplyConfiguration v1.LabelSelectorApplyConfiguration
+
+// +kubebuilder:object:root=true
+type TaskSpecApplyConfiguration corev1.PodSpecApplyConfiguration
 
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
 // TaskSpec defines the desired state of Task
+// +kubebuilder:object:root=true
 type TaskSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
-	// Foo is an example field of Task. Edit task_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	Selector *TaskLabelSelectorApplyConfiguration `json:"selector,omitempty"`
+	// +kubebuilder:validation:Required
+	Template *TaskSpecApplyConfiguration `json:"template,omitempty"`
 }
 
 // TaskStatus defines the observed state of Task
